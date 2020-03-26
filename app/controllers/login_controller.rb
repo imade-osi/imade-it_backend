0279c3aeb17c8c8f9ec9f7d0db6c1df780db3d4a
Imade Osifo <toxsik88@gmail.com>
2020-03-25 23:46:35 -0400

    removed excessive login errors

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -7,10 +7,13 @@ class LoginController < ApplicationController
       if user && user.authenticate(params[:password])
         
         render json: {token: make_token(user), id: user.id, name: user.name}
-      elsif user 
-        render json:{errors: ["Wrong Password, please try again!"]}, status: :unprocessable_entity
+      # elsif user 
+      #   render json:{errors: ["Wrong Password, please try again!"]}, status: :unprocessable_entity
+      # else 
+      #   render json:{errors: ["Hello Stranger! Please Sign up!"]}, status: :unprocessable_entity
+
       else 
-        render json:{errors: ["Hello Stranger! Please Sign up!"]}, status: :unprocessable_entity
+        nil 
       end 
       
     end
